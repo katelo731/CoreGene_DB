845eba0c36ca75e0ff871714a64e16c1cf32e9f1
Kate Lo <xe870731@gmail.com>
2018-08-10 18:01:25 +0800

    delete paralog results in raw data

--- Delete_paralog.cpp
+++ Delete_paralog.cpp
@@ -2,9 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
-
 using namespace std;
-
 int main(){
 	
 	const string input = "fungiresultDB.m8", output = "fungiresultDB.m8.out";
@@ -13,45 +11,44 @@ int main(){
 	START = clock();	// clock start
 	
 	ifstream in(input);
-    string inputStr;
-    vector<string> inputContent;
-    char id[6]={};	// id[6]= {id1[0],..,id1[2],id2[0],..,id2[2]}
+    	string inputStr;
+    	vector<string> inputContent;
+    	char id[6]={};		// id[6]= {id1[0],..,id1[2],id2[0],..,id2[2]}
     
-    while(getline(in, inputStr)){
+    	while(getline(in, inputStr)){
     	
-    	int c=0;
-    	int flag=0;
-    	
-    	for(int i=0;i<inputStr.size();i++)
-    	{
-    		if(c>=6) break;
-    		if(isalpha(inputStr[i]))
-    		{
-    			id[c]=inputStr[i];
-    			c++;
+		int c=0;
+		int flag=0;
+
+		for(int i=0;i<inputStr.size();i++)
+		{
+			if(c>=6) break;
+			if(isalpha(inputStr[i]))
+			{
+				id[c]=inputStr[i];
+				c++;
 			}
 		}
-    	
-    	for(int i=0;i<3;i++)
-    		if(id[i]!=id[i+3]) 
-			{
-				flag=1;
-				break;
-			}
-		
-		if(flag==1)
-        	inputContent.push_back(inputStr);
+
+		for(int i=0;i<3;i++)
+			if(id[i]!=id[i+3]) 
+				{
+					flag=1;
+					break;
+				}
+
+			if(flag==1)
+				inputContent.push_back(inputStr);
     }
     in.close();
 
     ofstream out(output);
     for(int i=0; i < inputContent.size(); i++)
-        out << inputContent[i] << endl;
+    	out << inputContent[i] << endl;
     out.close();
     
-	END = clock();    // clock end
-
-	cout << endl << "Execution time : " << (END - START) / CLOCKS_PER_SEC << " sec" << endl;
+    END = clock();    // clock end
+    cout << endl << "Execution time : " << (END - START) / CLOCKS_PER_SEC << " sec" << endl;
 
     return 0;
 }
