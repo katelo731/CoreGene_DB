c83c7154d622028b7840c6894ed945642cbdd224
Kate Lo <xe870731@gmail.com>
2018-08-16 15:21:24 +0800

    Update Delete_paralog.cpp

--- Delete_paralog.cpp
+++ Delete_paralog.cpp
@@ -5,49 +5,48 @@
 using namespace std;
 int main(){
 	
-	const string input = "fungiresultDB.m8", output = "fungiresultDB.m8.out";
+    double START,END;
+    START = clock();	// clock start
 	
-	double START,END;
-	START = clock();	// clock start
-	
-	ifstream in(input);
-    	string inputStr;
-    	vector<string> inputContent;
-    	char id[6]={};		// id[6]= {id1[0],..,id1[2],id2[0],..,id2[2]}
+    ifstream in("5mul5resultDBm8");
+    string inputStr;
+    vector<string> inputContent;
+    char id[6]={};	// id[6]= {id1[0],..,id1[2],id2[0],..,id2[2]}
     
-    	while(getline(in, inputStr)){
+    while(getline(in, inputStr)){
     	
-		int c=0;
-		int flag=0;
-
-		for(int i=0;i<inputStr.size();i++)
-		{
-			if(c>=6) break;
-			if(isalpha(inputStr[i]))
-			{
-				id[c]=inputStr[i];
-				c++;
-			}
-		}
-
-		for(int i=0;i<3;i++)
-			if(id[i]!=id[i+3]) 
-				{
-					flag=1;
-					break;
-				}
-
-			if(flag==1)
-				inputContent.push_back(inputStr);
+    int c=0;
+    int flag=0;
+    	
+    for(int i=0;i<inputStr.size();i++)
+    {
+        if(c>=6) break;
+        if(isalpha(inputStr[i]))
+        {
+            id[c]=inputStr[i];
+            c++;
+        }
+    }
+    	
+    for(int i=0;i<3;i++)
+        if(id[i]!=id[i+3]) 
+        {
+            flag=1;
+            break;
+        }
+		
+    if(flag==1)
+        inputContent.push_back(inputStr);
     }
     in.close();
 
-    ofstream out(output);
+    ofstream out("resultDBm8.out");
     for(int i=0; i < inputContent.size(); i++)
-    	out << inputContent[i] << endl;
+        out << inputContent[i] << endl;
     out.close();
     
     END = clock();    // clock end
+
     cout << endl << "Execution time : " << (END - START) / CLOCKS_PER_SEC << " sec" << endl;
 
     return 0;
